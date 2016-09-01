8c87fa90c8552db1913255e9b82e9902f8704f8b
nachomacaso <nathanmacaso@gmail.com>
2016-09-01 18:04:27 -0500

    Added forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,12 @@ class ProductsController < ApplicationController
   def all_sneakers
     @sneakers = Sneaker.all
   end
+
+  def sneaker_form
+
+  end
+
+  def display_sneaker_form
+    @sneaker = Sneaker.new(make: params[:product_make], model: params[:product_model], image: params[:product_image], description: [:product_description], price: params[:product_price])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'products#home'
   get '/single_product' => 'products#one_sneaker'
   get '/all_products' => 'products#all_sneakers'
+  get '/product_form' => 'products#sneaker_form'
+  post '/display_form' => 'products#display_sneaker_form'
 end
