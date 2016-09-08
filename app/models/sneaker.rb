1d7fef236aeefd2f5265920884bfed50473a5894
nachomacaso <nathanmacaso@gmail.com>
2016-09-07 21:30:52 -0500

    wednesday edits

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,2 +1,22 @@
 class Sneaker < ApplicationRecord
+  # def sale_message
+  #   if price.to_f < 1000
+  #     message = "Discounted Item!"
+  #   else
+  #     message = "On Sale!"
+  #   end
+  #   message
+  # end
+
+  def discounted?
+    price.to_f < 1000
+  end
+
+  def tax
+    tax = price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
 end
