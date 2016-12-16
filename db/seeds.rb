4e14dba91a4b2d654a7db3cdae13d909829b4ba3
nachomacaso <nathanmacaso@gmail.com>
2016-12-16 17:19:20 -0600

    Arranged seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
-CartedProduct.create!([
-  {user_id: 1, sneaker_id: 3, quantity: 1, status: "purchased", order_id: 1},
-  {user_id: 1, sneaker_id: 2, quantity: 1, status: "purchased", order_id: 2}
+Supplier.create!([
+  {name: "Flight Club", email: "flightclub@gmail.com", phone: "(602)555-7839"},
+  {name: "Kream", email: "kream@gmail.com", phone: "(312)555-1479"},
+  {name: "Foot Locker", email: "supplier@footlocker.com", phone: "(800)555-3478"}
+])
+Sneaker.create!([
+  {make: "Asics", model: "GT-2", description: "Asics GT-2 \"Rose Gold\"", price: "1100.0", sneaker_in_stock: true, supplier_id: 1},
+  {make: "Nike", model: "Air Jordan", description: "Air Jordan I Retro High OG Chicago (White/Black/Varsity Red)", price: "500.0", sneaker_in_stock: true, supplier_id: 2},
+  {make: "Nike", model: "Air Mag", description: "Nike Air Mag 'Back to the Future'", price: "30000.0", sneaker_in_stock: true, supplier_id: 3},
+  {make: "adidas", model: "NMD Primeknit", description: "adidas Originals NMD Primeknit 'Friends and Family'", price: "800.0", sneaker_in_stock: true, supplier_id: 1},
+  {make: "adidas", model: "yeezy boost 750", description: "adidas yeezy boost 750 Gray/Gum", price: "1200.0", sneaker_in_stock: true, supplier_id: 2},
+  {make: "New Balance", model: "ML999", description: "New Balance ML999 \"The Kennedy\"", price: "1000.0", sneaker_in_stock: true, supplier_id: 3},
+  {make: "Nike ", model: "Air Yeezy 2", description: "Nike Air Yeezy 2 \"Red October\"", price: "6000.0", sneaker_in_stock: true, supplier_id: 1},
+  {make: "adidas", model: "NMD", description: "Bape X adidas NMD \"Green Camo\" ", price: "600.0", sneaker_in_stock: true, supplier_id: 2},
+  {make: "Nike", model: "Dunk Hi", description: "DUNK HIGH SB PREMIUM \"MF DOOM\"", price: "400.0", sneaker_in_stock: true, supplier_id: 3}
+])
+User.create!([
+  {name: "nate", email: "nate@gmail.com", password_digest: "$2a$10$VBR4xDq1.rclJTXJ8ryWuOCEIpiKBci0dwU93DRMQLLacIDKnxxVK", admin: true}
 ])
 Category.create!([
   {name: "Adidas"},
@@ -56,22 +71,7 @@ Order.create!([
   {user_id: 1, subtotal: "30000.0", tax: "2700.0", total: "32700.0"},
   {user_id: 1, subtotal: "500.0", tax: "45.0", total: "545.0"}
 ])
-Sneaker.create!([
-  {make: "Asics", model: "GT-2", description: "Asics GT-2 \"Rose Gold\"", price: "1100.0", sneaker_in_stock: true, supplier_id: 1},
-  {make: "Nike", model: "Air Jordan", description: "Air Jordan I Retro High OG Chicago (White/Black/Varsity Red)", price: "500.0", sneaker_in_stock: true, supplier_id: 2},
-  {make: "Nike", model: "Air Mag", description: "Nike Air Mag 'Back to the Future'", price: "30000.0", sneaker_in_stock: true, supplier_id: 3},
-  {make: "adidas", model: "NMD Primeknit", description: "adidas Originals NMD Primeknit 'Friends and Family'", price: "800.0", sneaker_in_stock: true, supplier_id: 1},
-  {make: "adidas", model: "yeezy boost 750", description: "adidas yeezy boost 750 Gray/Gum", price: "1200.0", sneaker_in_stock: true, supplier_id: 2},
-  {make: "New Balance", model: "ML999", description: "New Balance ML999 \"The Kennedy\"", price: "1000.0", sneaker_in_stock: true, supplier_id: 3},
-  {make: "Nike ", model: "Air Yeezy 2", description: "Nike Air Yeezy 2 \"Red October\"", price: "6000.0", sneaker_in_stock: true, supplier_id: 1},
-  {make: "adidas", model: "NMD", description: "Bape X adidas NMD \"Green Camo\" ", price: "600.0", sneaker_in_stock: true, supplier_id: 2},
-  {make: "Nike", model: "Dunk Hi", description: "DUNK HIGH SB PREMIUM \"MF DOOM\"", price: "400.0", sneaker_in_stock: true, supplier_id: 3}
-])
-Supplier.create!([
-  {name: "Flight Club", email: "flightclub@gmail.com", phone: "(602)555-7839"},
-  {name: "Kream", email: "kream@gmail.com", phone: "(312)555-1479"},
-  {name: "Foot Locker", email: "supplier@footlocker.com", phone: "(800)555-3478"}
-])
-User.create!([
-  {name: "nate", email: "nate@gmail.com", password_digest: "$2a$10$VBR4xDq1.rclJTXJ8ryWuOCEIpiKBci0dwU93DRMQLLacIDKnxxVK", admin: true}
+CartedProduct.create!([
+  {user_id: 1, sneaker_id: 3, quantity: 1, status: "purchased", order_id: 1},
+  {user_id: 1, sneaker_id: 2, quantity: 1, status: "purchased", order_id: 2}
 ])
